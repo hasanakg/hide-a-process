11ca880c4995f3c4ac994528a25a4b7d378bb598
Hasan Akgün <hasanakg@outlook.com>
2013-11-17 23:06:17 +0200

    Rebuild test code.

--- test.c
+++ test.c
@@ -1,19 +1,71 @@
+/*************************************************************************
+ * Some test code to test system call 'set_hidden' does its job or not.  *
+ * 															17.11.2013   *
+ *************************************************************************/
+
 #include <stdio.h>
 #include <unistd.h>
 #define NR_set_hidden 351
 
 int main (void)
 {
-	 pid_t pid = getpid();
-	 int y = syscall(NR_set_hidden, pid,0);
-	 printf ("First Process ID: %d\n",pid);
-	 
-	 int testid = fork();
-	 
-	 if(testid == -1)
-		printf("No permission to fork or another error on forking. \n");
-	 else
-		printf("Process id: %d\n",testid);
+	 int choice;
+	 pid_t pid;
+	 printf("Enter (1) to fork current process or (2) to change a specific process' hidden flag. (0) to exit. ");
+	 scanf("%d",&choice);
+
+	 switch (choice)
+	 {
+		 case 0:
+		 {
+			printf("Good bye...\n");
+			return 0;
+		 }
+		 break;
+		 case 1: 
+		 {
+			pid = getpid();
+			int frk = fork();
+			if(frk == -1)
+			{
+				printf("Process couldn't fork succesfully. You have no permission to do this operation.\n");
+				return -1;
+			}	
+			else if (frk == 0)
+			{
+				printf("I'm the child process and my pid is %d\n",getpid());
+				return 0;
+			}
+			else
+			{
+				printf("I'm the parent process and my pid is %d\n",pid);
+				return 0;
+			}
+		 }
+		 break;
+		 case 2:
+		 {
+			int flag;
+			printf("Please enter pid of process you want to change its flag: "); 
+			scanf("%d",&pid);
+			printf("Please enter 'hidden' flag value: ");
+			scanf("%d",&flag);
+			long ret = syscall(NR_set_hidden, pid,flag);
+			if (ret != 0)
+			{
+				printf("Hidden flag couldn't changed succesfully, error code is %ld.\n",ret);
+				return -1;
+			}
+			else
+			{
+				printf("Hidden flag has been changed succesfully.\n");
+				return 0;				
+			}
+		 }
+		 break;
+		 default:
+			 return 0 ;
+	 }
+
      
-     return 0 ;
 }
